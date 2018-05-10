03f81b1c85d5e2b8017694fcd92ca2b6d12674cc
adrianmg89 <a.marcos.g989@gmail.com>
2018-05-10 16:21:15 +0200

    16.20 10-5-2018

---
+++ app/src/main/java/com/vpfc18/vpfc18/Principal/Asistido/Principal/Asistido_Botoneras_Fragment.java
@@ -0,0 +1,32 @@
+package com.vpfc18.vpfc18.Principal.Asistido.Principal;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.vpfc18.vpfc18.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Asistido_Botoneras_Fragment extends Fragment {
+
+
+    public Asistido_Botoneras_Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View vista = inflater.inflate(R.layout.fragment_asistido__botoneras_, container, false);
+
+        return vista;
+    }
+
+}
