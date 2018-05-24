d716a9efe63de65a059ebdf36f8daf85c80032ec
adrian <a.marcos.g989@gmail.com>
2018-05-24 11:00:03 +0200

    Mapa para android 7 10.58 24-5-2108

--- app/src/main/java/com/vpfc18/vpfc18/Principal/Voluntario/Principal/Voluntario_Principal_Activity.java
+++ app/src/main/java/com/vpfc18/vpfc18/Principal/Voluntario/Principal/Voluntario_Principal_Activity.java
@@ -1,7 +1,12 @@
 package com.vpfc18.vpfc18.Principal.Voluntario.Principal;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -16,12 +21,14 @@ import android.view.View;
 import com.vpfc18.vpfc18.Principal.Voluntario.Perfil.Voluntario_Perfil_Fragment;
 import com.vpfc18.vpfc18.R;
 
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+
 public class Voluntario_Principal_Activity extends AppCompatActivity {
 
     android.support.v7.widget.Toolbar voluntario_principal_toolbar;
     int contador=0;
     int perfil = 0;
-
+    int STORAGE_PERMISSION_CODE=2;
     String correoUser;
     @SuppressLint("NewApi")
     @Override
@@ -76,8 +83,7 @@ public class Voluntario_Principal_Activity extends AppCompatActivity {
 
     private void cargaMapa(){
         perfil=0;
-        voluntario_principal_toolbar.setNavigationIcon(R.drawable.ic_perfil);
-        getSupportActionBar().setTitle("Mapa eventos");
+        requestStoragePermission();
         Fragment fragmentoSeleccionado = new Voluntario_Mapa_Fragment();
         FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction t = fm.beginTransaction();
@@ -103,5 +109,54 @@ public class Voluntario_Principal_Activity extends AppCompatActivity {
         FragmentTransaction t = fm.beginTransaction();
         t.replace(R.id.voluntario_contenedor_principal, fragmentoSeleccionado);
         t.commit();
+    }
+    public void requestStoragePermission() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                voluntario_principal_toolbar.setNavigationIcon(R.drawable.ic_perfil);
+                getSupportActionBar().setTitle("Mapa eventos");
+            }
+        });
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                ActivityCompat.requestPermissions(Voluntario_Principal_Activity.this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, STORAGE_PERMISSION_CODE);
+            } else {
+                ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, STORAGE_PERMISSION_CODE);
+            }
+        }
+        return;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == STORAGE_PERMISSION_CODE)  {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                gohome();
+            } else {
+
+            }
+        }
+    }
+
+    public void gohome(){
+        Thread t = new Thread(){
+            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+            @Override
+            public void run() {
+                try {
+                    sleep(700);
+                }catch (Exception e){
+
+                } finally {
+                    cargaMapa();
+                }
+            }
+        };
+        t.start();
+
+
     }
 }
