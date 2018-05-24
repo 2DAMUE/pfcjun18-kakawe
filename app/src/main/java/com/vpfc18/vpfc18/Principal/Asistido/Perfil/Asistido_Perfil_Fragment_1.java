6a9fae9f905c3688d67a2de50e96230c9cc6405b
McCrash <eloymccrash@gmail.com>
2018-05-24 19:01:03 +0100

    Botón habilitar campos

--- app/src/main/java/com/vpfc18/vpfc18/Principal/Asistido/Perfil/Asistido_Perfil_Fragment_1.java
+++ app/src/main/java/com/vpfc18/vpfc18/Principal/Asistido/Perfil/Asistido_Perfil_Fragment_1.java
@@ -9,9 +9,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.vpfc18.vpfc18.Principal.Asistido.Principal.Asistido_Principal_Activity;
 import com.vpfc18.vpfc18.R;
@@ -19,13 +21,13 @@ import com.vpfc18.vpfc18.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Asistido_Perfil_Fragment_1 extends Fragment{
+public class Asistido_Perfil_Fragment_1 extends Fragment {
 
-    EditText et_perfil_email,et_perfil_telefono,et_perfil_nombre,et_perfil_apellido;
-    Button btn_perfil_guardar;
-    TextView tv_perfil_modContrasena,tv_perfil_datosMedicos;
+    EditText et_perfil_email, et_perfil_telefono, et_perfil_nombre, et_perfil_apellido, et_perfil_fnacimiento, et_perfil_sexo;
+    ToggleButton btn_perfil_guardar;
+    TextView tv_perfil_modContrasena, tv_perfil_datosMedicos;
 
-    String email,contrasena,nuevaContrasena,nombre,apellido,telefono;
+    String email, contrasena, nuevaContrasena, nombre, apellido, telefono;
 
     public Asistido_Perfil_Fragment_1() {
         // Required empty public constructor
@@ -36,29 +38,42 @@ public class Asistido_Perfil_Fragment_1 extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View vista = inflater.inflate(R.layout.asistido_fragment_perfil_1, container, false);
-        et_perfil_email = (EditText)vista.findViewById(R.id.et_perfil_email);
-        et_perfil_telefono = (EditText)vista.findViewById(R.id.et_perfil_telefono);
-        et_perfil_nombre = (EditText)vista.findViewById(R.id.et_perfil_nombre);
-        et_perfil_apellido = (EditText)vista.findViewById(R.id.et_perfil_apellido);
-        btn_perfil_guardar = (Button) vista.findViewById(R.id.btn_perfil_guardar);
+        btn_perfil_guardar = (ToggleButton) vista.findViewById(R.id.btn_perfil_guardar);
+        et_perfil_nombre = (EditText) vista.findViewById(R.id.et_perfil_nombre);
+        et_perfil_apellido = (EditText) vista.findViewById(R.id.et_perfil_apellido);
+        et_perfil_fnacimiento = (EditText) vista.findViewById(R.id.et_perfil_fnacimiento);
+        et_perfil_sexo = (EditText) vista.findViewById(R.id.et_perfil_sexo);
+
+        et_perfil_telefono = (EditText) vista.findViewById(R.id.et_perfil_telefono);
+        et_perfil_email = (EditText) vista.findViewById(R.id.et_perfil_email);
+
         tv_perfil_modContrasena = (TextView) vista.findViewById(R.id.tv_perfil_modContrasena);
         tv_perfil_datosMedicos = (TextView) vista.findViewById(R.id.tv_perfil_datosMedicos);
 
-        btn_perfil_guardar.setOnClickListener(new View.OnClickListener() {
+
+        btn_perfil_guardar.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View v) {
-                if (comprobarCampos()){
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    habilitarCampos(true);
+                } else if (comprobarCampos()) {
+                    habilitarCampos(false);
                     actualizarDatos();
                 }
             }
         });
-        tv_perfil_modContrasena.setOnClickListener(new View.OnClickListener() {
+
+        tv_perfil_modContrasena.setOnClickListener(new View.OnClickListener()
+
+        {
             @Override
             public void onClick(View v) {
                 modificarContrasena();
             }
         });
-        tv_perfil_datosMedicos.setOnClickListener(new View.OnClickListener() {
+        tv_perfil_datosMedicos.setOnClickListener(new View.OnClickListener()
+
+        {
             @Override
             public void onClick(View v) {
                 vistaDatosMedicos();
@@ -68,23 +83,36 @@ public class Asistido_Perfil_Fragment_1 extends Fragment{
     }
 
 
-    private void actualizarDatos(){
+    private void actualizarDatos() {
 
     }
-    private boolean comprobarCampos(){
+
+
+    private void habilitarCampos(Boolean habilitado) {
+        et_perfil_nombre.setEnabled(habilitado);
+        et_perfil_apellido.setEnabled(habilitado);
+        et_perfil_fnacimiento.setEnabled(habilitado);
+        et_perfil_sexo.setEnabled(habilitado);
+        et_perfil_telefono.setEnabled(habilitado);
+        et_perfil_email.setEnabled(habilitado);
+
+    }
+
+
+    private boolean comprobarCampos() {
         email = et_perfil_email.getText().toString().trim();
         telefono = et_perfil_telefono.getText().toString();
         nombre = et_perfil_nombre.getText().toString();
         apellido = et_perfil_apellido.getText().toString();
 
-        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-            String a= "Escribe un correo válido";
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            String a = "Escribe un correo válido";
             Toast.makeText(getContext(), a, Toast.LENGTH_LONG).show();
             et_perfil_email.requestFocus();
             return false;
         }
-        if (telefono.isEmpty()){
-            String a= "No puedes dejar el campo teléfono vacio";
+        if (telefono.isEmpty()) {
+            String a = "No puedes dejar el campo teléfono vacio";
             Toast.makeText(getContext(), a, Toast.LENGTH_LONG).show();
             et_perfil_telefono.requestFocus();
             return false;
