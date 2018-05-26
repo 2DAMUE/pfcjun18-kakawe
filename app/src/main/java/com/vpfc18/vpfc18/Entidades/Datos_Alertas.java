47d0400b1045c76877c2212daa996c44c18605fc
Jose <josedfa86@gmail.com>
2018-05-26 13:00:12 +0200

    creacion de objeto marker desde la bbdd y obtención de metodo notificación

--- app/src/main/java/com/vpfc18/vpfc18/Entidades/Datos_Alertas.java
+++ app/src/main/java/com/vpfc18/vpfc18/Entidades/Datos_Alertas.java
@@ -3,16 +3,14 @@ package com.vpfc18.vpfc18.Entidades;
 public class Datos_Alertas {
 
     private String id_dependiente;
-    private String id_tipoAlerta;
     private double latitud;
     private double logitud;
 
     public Datos_Alertas() {
     }
 
-    public Datos_Alertas(String id_dependiente, String id_tipoAlerta, double latitud, double logitud) {
+    public Datos_Alertas(String id_dependiente, double latitud, double logitud) {
         this.id_dependiente = id_dependiente;
-        this.id_tipoAlerta = id_tipoAlerta;
         this.latitud = latitud;
         this.logitud = logitud;
     }
@@ -23,14 +21,6 @@ public class Datos_Alertas {
 
     public void setId_dependiente(String id_dependiente) {
         this.id_dependiente = id_dependiente;
-    }
-
-    public String getId_tipoAlerta() {
-        return id_tipoAlerta;
-    }
-
-    public void setId_tipoAlerta(String id_tipoAlerta) {
-        this.id_tipoAlerta = id_tipoAlerta;
     }
 
     public double getLatitud() {
@@ -47,5 +37,14 @@ public class Datos_Alertas {
 
     public void setLogitud(double logitud) {
         this.logitud = logitud;
+    }
+
+    @Override
+    public String toString() {
+        return "Datos_Alertas{" +
+                "id_dependiente='" + id_dependiente + '\'' +
+                ", latitud=" + latitud +
+                ", logitud=" + logitud +
+                '}';
     }
 }
