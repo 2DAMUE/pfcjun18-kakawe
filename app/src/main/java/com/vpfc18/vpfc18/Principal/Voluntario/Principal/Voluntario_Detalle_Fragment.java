6c9b68ee77f8b30de6e504611c9506e3fa705af6
McCrash <eloymccrash@gmail.com>
2018-06-11 18:55:57 +0200

    Layouts editados

--- app/src/main/java/com/vpfc18/vpfc18/Principal/Voluntario/Principal/Voluntario_Detalle_Fragment.java
+++ app/src/main/java/com/vpfc18/vpfc18/Principal/Voluntario/Principal/Voluntario_Detalle_Fragment.java
@@ -29,6 +29,7 @@ public class Voluntario_Detalle_Fragment extends Fragment {
         // Required empty public constructor
     }
 
+    
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
