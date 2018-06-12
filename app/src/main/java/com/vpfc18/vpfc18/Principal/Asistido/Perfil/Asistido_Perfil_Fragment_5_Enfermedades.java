2d1e2401d2132b2cda108593ade5eae51d656c35
Jose <josedfa86@gmail.com>
2018-06-12 21:16:31 +0200

    Creación de fragmentos en voluntario y asistido con botones

--- app/src/main/java/com/vpfc18/vpfc18/Principal/Asistido/Perfil/Asistido_Perfil_Fragment_5_Enfermedades.java
+++ app/src/main/java/com/vpfc18/vpfc18/Principal/Asistido/Perfil/Asistido_Perfil_Fragment_5_Enfermedades.java
@@ -3,9 +3,11 @@ package com.vpfc18.vpfc18.Principal.Asistido.Perfil;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.vpfc18.vpfc18.R;
 
@@ -14,6 +16,10 @@ import com.vpfc18.vpfc18.R;
  */
 public class Asistido_Perfil_Fragment_5_Enfermedades extends Fragment {
 
+
+    String correoUser;
+    View vista;
+    Button bt_perfil_asistido_enfermedades_guardar;
 
     public Asistido_Perfil_Fragment_5_Enfermedades() {
         // Required empty public constructor
@@ -24,7 +30,24 @@ public class Asistido_Perfil_Fragment_5_Enfermedades extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.asistido_fragment_perfil_enfermedades, container, false);
+        vista = inflater.inflate(R.layout.asistido_fragment_perfil_enfermedades, container, false);
+
+        bt_perfil_asistido_enfermedades_guardar = (Button)vista.findViewById(R.id.bt_perfil_asistido_enfermedades_guardar);
+        bt_perfil_asistido_enfermedades_guardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragmentoSeleccionado = new Asistido_Perfil_Fragment_1();
+                FragmentTransaction t = getFragmentManager().beginTransaction();
+                t.replace(R.id.contenedor_perfil_asistido, fragmentoSeleccionado);
+                t.commit();
+                Bundle datos = new Bundle();
+                datos.putString("correoUser", correoUser);
+                fragmentoSeleccionado.setArguments(datos);
+            }
+        });
+
+
+        return vista;
     }
 
 }
